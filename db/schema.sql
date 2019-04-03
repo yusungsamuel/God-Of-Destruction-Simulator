23082f683d4048b49340cdcad978172385bb6381
yusungsamuel <ssyyu@ucdavis.edu>
2019-04-02 18:42:50 -0700

    add code to schema.sql to create database and add code to seeds.sql to insert data

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE dragonBallSuper_db;
+
+USE dragonBallSuper_db;
+
+CREATE TABLE characters (
+    id INT AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    name VARCHAR(100) NOT NULL,
+    destroyed BOOLEAN
+);
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO characters(name, destroyed)
+VALUES("Zamasu", false), ("Goku", false), ("Vegeta", false);
