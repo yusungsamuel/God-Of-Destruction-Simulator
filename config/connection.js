841266ba9ed2abb998b77f3f360bc039662e4389
yusungsamuel <ssyyu@ucdavis.edu>
2019-04-05 10:32:51 -0700

    fix background image with min height 100vh

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
-  mysql.createConnection({
+  connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     port: 3306,
@@ -15,7 +15,8 @@ else {
   });
 };
 
-connection.connect(function (err) {
+connection.connect(
+  function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
