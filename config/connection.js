c7a377d8f4bd4c8130b23cab54ad8822543b80e1
yusungsamuel <ssyyu@ucdavis.edu>
2019-04-05 09:52:50 -0700

    add jawDB info

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,26 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  mysql.createConnection({
     host: 'localhost',
     user: 'root',
     port: 3306,
     password: 'password',
     database: 'dragonBallSuper_db'
+  });
+};
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
-connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
-
-  module.exports = connection;
+module.exports = connection;
